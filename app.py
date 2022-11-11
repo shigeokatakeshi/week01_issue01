04c717094eaa46bc0dcb48dac29564b5afd074b0
shigeokatakeshi <takeshi0713@gmail.com>
2022-11-11 18:31:12 +0900

    修正しました

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from PIL import ImageGrab
 import pyautogui
-import pyautogui as pag
 import pyperclip
 import time
 import webbrowser
@@ -12,9 +11,8 @@ pyperclip.copy("平泉町 観光")
 pyautogui.hotkey("command", "v")
 pyautogui.hotkey("\n")
 time.sleep(1)
-screenshot = ImageGrab.grab()
+pyautogui.screenshot()
+pyautogui.screenshot("image.png")
 time.sleep(1)
 screenshot.save("screenshot.png")
 # pyautogui.hotkey("command", "shift", "3")
-
-
